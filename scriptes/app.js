c0ed89a7f97360fafe586a9bf387732cc109b61b
Forhadul Islam <forhadul75@gmail.com>
2025-02-27 23:53:07 +0600

    complete this project

--- scriptes/app.js
+++ scriptes/app.js
@@ -1,39 +1,10 @@
 
 // Card 1 history 
-document.getElementById("historyBTn").addEventListener('click', function () {
-    document.getElementById("historyBTn").style.backgroundColor = "#b4f461";
-    document.getElementById("donateBTn").classList.add('border-2', 'border-secondary', 'bg-white');
-    console.log(getInputValue('card1Amount'));
-
-    // History show
-    const historySection = document.getElementById("historySection")
-    const div = document.createElement('div');
-    div.innerHTML = `
-         <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
-            <h5 class="font-bold">${getInputValue('card1Amount')} Taka is ${getInnerText('card1Title')}</h5>
-            <p class="text-gray-400">Date : ${new Date()}</p>
-        </div>
-    `
-    historySection.appendChild(div);
-
-    // -------------------
-    div.innerHTML = `
-         <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
-            <h5 class="font-bold">${getInputValue('card2Amount')} Taka is ${getInnerText('card2Title')}</h5>
-            <p class="text-gray-400">Date : ${new Date()}</p>
-        </div>
-    `
-    historySection.appendChild(div);
-
-    // -------------------------------
-    div.innerHTML = `
-         <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
-            <h5 class="font-bold">${getInputValue('card3Amount')} Taka is ${getInnerText('card3Title')}</h5>
-            <p class="text-gray-400">Date : ${new Date()}</p>
-        </div>
-    `
-    historySection.appendChild(div);
-
+document.getElementById("historyBtn").addEventListener('click', function () {
+    document.getElementById('historySection').classList.remove('hidden');
+    
+    document.getElementById("historyBtn").style.backgroundColor = "#b4f461";
+    document.getElementById("donateBtn").classList.add('border-2', 'border-secondary', 'bg-white');
 
 
 
@@ -42,8 +13,25 @@ document.getElementById("historyBTn").addEventListener('click', function () {
 
 
 // transaction 
-document.getElementById('donateBTn').addEventListener('click', function () {
-    document.getElementById("donateBTn").style.backgroundColor = "#b4f461";
+document.getElementById('donateBtn').addEventListener('click', function () {
     document.getElementById("historySection").style.display = 'none';
-    document.getElementById("historyBTn").style.backgroundColor = 'white'
+
+    document.getElementById("donateBtn").style.backgroundColor = "#b4f461";
+    document.getElementById("historyBtn").style.backgroundColor = "#fff";
+    
+
 })
+
+
+/* const buttons = document.querySelectorAll('.buttons');
+for (let btn of buttons) {
+    btn.addEventListener('click', function (event) {
+
+        event.target.classList.add('border-2', 'border-secondary', 'bg-white');
+        
+
+
+    })
+
+
+} */
--- scriptes/donate.js
+++ scriptes/donate.js
@@ -3,31 +3,38 @@
 document.getElementById('card1Btn').addEventListener('click', function () {
     const card1Value = getInputValue('card1Amount');
     const mainBalance = getInnerText('mainBalance');
-    
+
 
 
     if (card1Value < mainBalance) {
         if (card1Value > 0) {
             const sum = mainBalance - card1Value;
             document.getElementById('mainBalance').innerText = sum;
-
             // Cars balance update
             const card1Balance = parseFloat(getInnerText('card1Balance'));
             document.getElementById('card1Balance').innerText = card1Balance + card1Value;
+            // add details in history
+            const historySection = document.getElementById("historySection")
+            const div = document.createElement('div');
+            div.innerHTML = `
+                     <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
+                        <h5 class="font-bold">${getInputValue('card1Amount')} Taka is ${getInnerText('card1Title')}</h5>
+                        <p class="text-gray-400">Date : ${new Date()}</p>
+                    </div>
+                `
+            historySection.appendChild(div);
+            // successfully donate popup
+            document.getElementById('successBox').classList.remove('hidden');
 
         }
         else {
             alert("Enter Positive value")
         }
-
     }
     else {
 
         alert("Insufficent Balance")
     }
-
-
-
 })
 
 // Card 2 Donate
@@ -43,7 +50,54 @@ document.getElementById('card2Btn').addEventListener('click', function () {
             // Cars balance update
             const card2Balance = parseFloat(getInnerText('card1Balance'));
             document.getElementById('card2Balance').innerText = card2Balance + card2Value;
+            // add details in history
+            const historySection = document.getElementById("historySection")
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
+               <h5 class="font-bold">${getInputValue('card2Amount')} Taka is ${getInnerText('card2Title')}</h5>
+               <p class="text-gray-400">Date : ${new Date()}</p>
+           </div>
+       `
+            historySection.appendChild(div);
+            // successfully donate popup
+            document.getElementById('successBox').classList.remove('hidden');
+        }
+        else {
+            alert("Enter Positive value")
+        }
+    }
+    else {
 
+        alert("Insufficent Balance")
+    }
+})
+
+// Card 2 Donate
+document.getElementById('card3Btn').addEventListener('click', function () {
+    const mainBalance = getInnerText('mainBalance');
+    const card3Value = getInputValue('card3Amount');
+
+    if (card3Value < mainBalance) {
+        if (card3Value > 0) {
+            const sum = mainBalance - card3Value;
+            document.getElementById('mainBalance').innerText = sum;
+            // Cars balance update
+            const card3Balance = parseFloat(getInnerText('card3Balance'));
+            document.getElementById('card3Balance').innerText = card3Balance + card3Value;
+
+            // add details in history
+            const historySection = document.getElementById("historySection")
+            const div = document.createElement('div');
+            div.innerHTML = `
+    <div class="p-5 mb-3 border-2 border-gray-300 rounded-lg space-y-2 w-11/12 mx-auto">
+       <h5 class="font-bold">${getInputValue('card3Amount')} Taka is ${getInnerText('card3Title')}</h5>
+       <p class="text-gray-400">Date : ${new Date()}</p>
+   </div>
+`
+            historySection.appendChild(div);
+            // successfully donate popup
+            document.getElementById('successBox').classList.remove('hidden');
         }
         else {
             alert("Enter Positive value")
@@ -57,29 +111,6 @@ document.getElementById('card2Btn').addEventListener('click', function () {
 
 })
 
-// Card 2 Donate
-document.getElementById('card3Btn').addEventListener('click', function () {
-    const mainBalance = getInnerText('mainBalance');
-    const card3Value = getInputValue('card3Amount');
-
-    if (card3Value < mainBalance) {
-        if (card3Value > 0) {
-            const sum = mainBalance - card3Value;
-            document.getElementById('mainBalance').innerText = sum;
-
-            // Cars balance update
-            const card3Balance = parseFloat(getInnerText('card3Balance'));
-            document.getElementById('card3Balance').innerText = card3Balance + cardValue;
-
-        }
-        else {
-            alert("Enter Positive value")
-        }
-
-    }
-    else {
-
-        alert("Insufficent Balance")
-    }
-
+document.getElementById('closeBtn').addEventListener('click', function(){
+    document.getElementById('successBox').classList.add('hidden');
 })
